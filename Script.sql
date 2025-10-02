b38a08fb70dcb097a91a4cef3917e25b007e7bee
irineu3232 <irineu32323@gmail.com>
2025-10-02 14:47:59 -0300

    Iniciando projeto Cinema

---
+++ Script.sql
@@ -0,0 +1,185 @@
+-- Criando database
+drop database if exists dbCinema;
+create database dbCinema;
+use dbCinema;
+
+
+create table Usuario (
+id int primary key auto_increment,
+Nome varchar(100) not null,
+Email varchar(150) not null,
+Senha varchar(100) not null,
+role enum ('admin','gerente','cliente'),
+Ativo varchar(1) not null
+);
+
+
+create table Diretores(
+id_diretor int primary key auto_increment,
+nome varchar(100) not null,
+pais_origem varchar(100) not null
+);
+
+create table Filmes(
+id_filme int primary key auto_increment,
+titulo varchar(100) not null,
+genero int,
+id_diretor int,
+foreign key (id_diretor) references Diretores(id_diretor)
+);
+
+create table Filmes_Genero(
+id_Gen int primary key auto_increment,
+nomeGen varchar(100)
+);
+
+
+create table Premiacoes(
+id_premiacao int primary key auto_increment,
+id_filme int,
+nomePremio varchar(100) not null,
+foreign key(id_filme) references Filmes(id_filme)
+);
+
+
+-- Criando procedures
+Delimiter $$
+create procedure cad_Usuario(u_nome varchar (100), u_email varchar(100), u_senha varchar(100), u_role varchar(40))
+begin
+
+	if not exists (select id from Usuario where Email = u_email)
+					then
+		insert into Usuario(Nome, Email, Senha, role, Ativo)
+						values(u_nome, u_email, u_senha, u_role, 1);
+	end if;
+end $$
+
+Delimiter $$
+create procedure editar_usuario(u_nome varchar (100), u_email varchar(100), u_senha varchar(100), u_role varchar(40), id_user int)
+begin
+			
+            Update Usuario
+            set Nome = u_nome, Email = u_email, Senha = u_senha, role = u_role
+            where id = id_user;
+
+end $$
+
+
+Delimiter $$
+create procedure deletar_usuario(u_id int)
+begin
+	delete from Usuario where id = u_id;
+end $$
+
+Delimiter $$
+create procedure buscar_usuario (u_email varchar(150))
+begin
+
+	select Id, Nome , Email, Senha, role, Ativo from Usuarios
+    where Email = u_email;
+
+end $$
+
+
+Delimiter $$
+create procedure listar_usuario ()
+begin
+	select Id, Nome, Email, Senha, role, Ativo from Usuario;
+end $$
+
+-- Criando Filmes
+describe Filmes;
+
+Delimiter $$
+create procedure cad_Filme(f_titulo varchar (100), f_genero int, f_diretor int)
+begin
+
+	if not exists (select id_filme from Filme where titulo = f_titulo)
+					then
+		insert into Filmes(titulo, genero, id_diretor)
+						values(f_titulo, f_genero, f_diretor);
+	end if;
+end $$
+
+Delimiter $$
+create procedure editar_filme(f_titulo varchar (100), f_genero int, f_diretor int, f_cod int)
+begin
+			
+            Update Filmes
+            set titulo = f_titulo, genero = f_genero, id_diretor = f_diretor
+            where id_filme = f_cod;
+
+end $$
+
+
+Delimiter $$
+create procedure deletar_filme(f_id int)
+begin
+	delete from Filmes where id_filme = f_id;
+end $$
+
+
+
+Delimiter $$
+create procedure listar_Filme()
+begin
+	select f.id_filme, f.titulo, g.nomeGen, d.nomeDiretor from Filmes f
+    inner join Diretores d on f.id_diretor = d.id_diretor
+    inner join Genero g on f.genero = g.id_gen;
+
+end $$
+
+
+
+
+-- Premiacao
+
+Delimiter $$
+create procedure cad_premiacao(p_nomePremio varchar (100), p_filme int)
+begin
+
+		insert into Premiacoes(id_filme, nomePremio)
+						values(p_filme, p_nomePremio);
+
+end $$
+
+Delimiter $$
+create procedure editar_premiacao(p_nomePremio varchar (100), p_filme int, p_id int)
+begin
+			
+            Update Premiacoes
+			set nomePremio = p_nomePremio, id_filme = p_filme
+            where id_premiacao = p_id;
+end $$
+
+
+Delimiter $$
+create procedure deletar_usuario(f_id int)
+begin
+	delete from Filmes where id_filme = f_id;
+end $$
+
+
+
+Delimiter $$
+create procedure listar_Filme(f_id int)
+begin
+	select p.id_premiacao, p.id_filme, f.titulo, p.nomePremio, g.id_Gen g.nomeGen, d.nome from Premiacoes p
+    inner join Filmes f  on p.id_filme = f.id_filme
+    inner join Diretores d on f.id_diretor = d.id_diretor
+    inner join Genero g on f.genero = g.id_gen;
+	where f.id_filme = f_id; 
+end $$
+
+
+
+
+
+
+
+describe Diretores;
+describe Filmes;
+describe Filmes_Genero;
+describe Premiacoes;
+
+
